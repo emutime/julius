e0cdbfe74d2c68d196e18515af1424dcad9fa219
emutime <emutime@qq.com>
2025-04-15 23:24:10 +0800

    【工具】枚举的值引用

--- tool/ast/core/CAstNode/EnumConstantDecl.ts
+++ tool/ast/core/CAstNode/EnumConstantDecl.ts
@@ -4,9 +4,11 @@ import { ConstantExpr } from "./ConstantExpr";
 
 export class EnumConstantDecl extends ASTNode {
     public m_nodeValue: ConstantExpr | undefined;
+    public readonly isReferenced: boolean;
     public constructor(node: Record<string, any>) {
         super(node);
         this.m_nodeValue = this.children.find(child => child.isKind(SynxType.ConstantExpr));
+        this.isReferenced = !!node["isReferenced"];
     }
     public getName(): string {
         return this.name!;
--- tool/ast/core/Transformers/TransformerEnumDecl.ts
+++ tool/ast/core/Transformers/TransformerEnumDecl.ts
@@ -1,4 +1,5 @@
-import type { SourceFile as SourceFileTS } from 'ts-morph';
+import path from 'path';
+import { type SourceFile as SourceFileTS } from 'ts-morph';
 import { ASTNode } from '../CAstNode/ASTNode';
 import type { SourceFile } from '../CAstNode/SourceFile';
 import { SynxType } from "../SynxType";
@@ -7,6 +8,37 @@ import { TransformerBase } from './TransformerBase';
 export class TransformerEnumDecl extends TransformerBase {
     public override transform(node: ASTNode, sourceFile: SourceFile, sourceFileTS: SourceFileTS) {
         if (!node.isKind(SynxType.EnumDecl)) {
+            return;
+        }
+
+        if (node.includedFrom) {
+            const referenced = node.getConstants().filter(item => item.isReferenced);
+            if (referenced.length === 0) {
+                return;
+            }
+
+            // 不是当前src文件夹下的enum, 不生成import
+            const baseDir = path.resolve(process.cwd() + '\\src');
+            if (node.locFile && !node.locFile.startsWith(baseDir)) {
+                return;
+            }
+
+            // 没有locFile, 不生成import
+            if (!node.locFile) {
+                return;
+            }
+
+            // 生成import
+            let removedExt = node.locFile.substring(0, node.locFile.length - path.extname(node.locFile).length);
+            let importPath = path.relative(baseDir, removedExt).replace(/\\/g, '/');
+
+            sourceFileTS.addImportDeclaration({
+                moduleSpecifier: importPath,
+                namedImports: [node.name!],
+            });
+
+            // 生成 import constant = enum.constant;
+            sourceFileTS.addStatements(referenced.map(item => `import ${item.getName()} = ${node.name!}.${item.getName()};`));
             return;
         }
 
